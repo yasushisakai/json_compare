b33813016d9ce962065f42625d30e9353b0f33d2
Yasushi Sakai <ysshski@gmail.com>
2020-05-17 17:56:30 -0400

    changed config and attributes

---
+++ json_std_conv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys
+import json
+
+file_name = sys.argv[1]
+
+file = open(file_name, 'r')
+data = json.loads(file.read())
+file.close()
+
+print(json.dumps(data, sort_keys=True, indent=2))
